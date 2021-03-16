ca77c17d10f58b734dee92d526bcf6686762dead
AQUAMAN <78766821+shahrozxdr@users.noreply.github.com>
2021-03-16 22:28:54 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -17,12 +17,12 @@ module.exports = {
     let helpEmbed = new MessageEmbed()
     .setDescription(`
 
-🔎 [Click here](https://discord.com/api/oauth2/authorize?client_id=811298712348262470&permissions=8&scope=bot) **to invite the bot.**
+🔗 [Click here](https://discord.com/api/oauth2/authorize?client_id=811298712348262470&permissions=8&scope=bot) **to invite the bot.**
 `)
 
   
    .setColor("YELLOW");
-   message.react("<:emoji_12: 821459760402399294>")
+   message.react("✅")
     return message.channel.send(helpEmbed).catch(console.error);
 
   }
