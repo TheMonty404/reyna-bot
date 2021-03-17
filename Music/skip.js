280d228f863db92c66fb8a3171740cde16de92a1
AQUAMAN <78766821+shahrozxdr@users.noreply.github.com>
2021-03-17 09:44:05 +0300

    Update skip.js

--- Music/skip.js
+++ Music/skip.js
@@ -33,7 +33,7 @@ execute(message) {
     queue.connection.dispatcher.end();
     //send approve message
     queue.textChannel.send(
-      new MessageEmbed().setColor("YELLOW").setAuthor(`${message.author.username} skipped the song.`, "")
+      new MessageEmbed().setColor("BLUE").setAuthor(`${message.author.username} skipped the song.`, "")
     ).catch(console.error);
   }
 };
