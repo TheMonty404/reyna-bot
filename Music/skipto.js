9a37e3ab7121ccd40b6cb6e29101b7f87657c7ba
AQUAMAN <78766821+shahrozxdr@users.noreply.github.com>
2021-03-17 09:48:26 +0300

    Update skipto.js

--- Music/skipto.js
+++ Music/skipto.js
@@ -52,7 +52,7 @@ execute(message, args) {
     //Send approve
     queue.textChannel.send(
       new MessageEmbed()
-        .setColor("YELLOW")
+        .setColor("RED")
         .setAuthor(`${message.author.username}#${message.author.discriminator} skipped ${args[0]} songs`, "")
     ).catch(console.error);
   }
