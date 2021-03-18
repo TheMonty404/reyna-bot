80cb931c7bcc45cb7aaf6e6994a6a2670cf5fe1f
AQUAMAN <78766821+shahrozxdr@users.noreply.github.com>
2021-03-19 02:37:30 +0300

    Update stop.js

--- Music/stop.js
+++ Music/stop.js
@@ -19,7 +19,7 @@ async execute(message,args,client) {
   //if not in a guild retunr
   if (!message.guild) return;
   //react with approve emoji
-  message.react("<emoji_4822203026776391711>").catch(console.error);
+  message.react("<:emoji_4:822203026776391711>").catch(console.error);
   const { channel } = message.member.voice;
   //get the serverQueue
   const queue = message.client.queue.get(message.guild.id);
