8e8149c6711874b133739d9ccb3a9177876b62b1
AQUAMAN <78766821+shahrozxdr@users.noreply.github.com>
2021-03-19 01:49:40 +0300

    Update search.js

--- Music/search.js
+++ Music/search.js
@@ -23,7 +23,7 @@ async execute(message,args,client) {
      //get serverqueue
      const serverQueue = message.client.queue.get(message.guild.id);
     //react with approve emoji
-    message.react("✅").catch(console.error);
+    message.react("<emoji_4822203026776391711>").catch(console.error);
     //if the argslength is null return error
     if (!args.length)
       return attentionembed(message,`Usage: ${message.client.prefix}${module.exports.name} <Video Name>`)
@@ -41,7 +41,7 @@ async execute(message,args,client) {
     //define a temporary Loading Embed
     let temEmbed = new MessageEmbed()
     .setAuthor("Searching...", "")
-    .setColor("GREEN")
+    .setColor("PURPLE")
     //define the Result Embed
     let resultsEmbed = new MessageEmbed()
       .setTitle("Results for: ")
