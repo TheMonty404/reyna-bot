2768f6692b87a7d303e57841759f9d1e503b51fe
AQUAMAN <78766821+shahrozxdr@users.noreply.github.com>
2021-03-19 01:52:19 +0300

    Update skipto.js

--- Music/skipto.js
+++ Music/skipto.js
@@ -19,7 +19,7 @@ execute(message, args) {
     //if not in a guild return
     if (!message.guild) return;
     //react with approve
-    message.react("✅").catch(console.error);
+    message.react("<emoji_4822203026776391711>").catch(console.error);
     //if no args return error
     if (!args.length)
       return attentionembed(message, `Try: ${message.client.prefix}${module.exports.name} <Queue Number>`)
@@ -52,7 +52,7 @@ execute(message, args) {
     //Send approve
     queue.textChannel.send(
       new MessageEmbed()
-        .setColor("RED")
+        .setColor("PURPLE")
         .setAuthor(`${message.author.username}#${message.author.discriminator} skipped ${args[0]} songs`, "")
     ).catch(console.error);
   }
