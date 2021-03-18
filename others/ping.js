d67bd453bd9799df209758d72f23ab5379fc1fe7
AQUAMAN <78766821+shahrozxdr@users.noreply.github.com>
2021-03-19 02:08:50 +0300

    Update ping.js

--- others/ping.js
+++ others/ping.js
@@ -13,8 +13,8 @@ module.exports = {
   edesc: "Type this command to see how fast the Bot can response to your messages / commands inputs!",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("✅");
+    message.react("<:emoji_4:822203026776391711>");
     //send the Ping embed
-    message.reply(new MessageEmbed().setColor("GREEN").setTitle("📶 `" + client.ws.ping + "ms`"));
+    message.reply(new MessageEmbed().setColor("PURPLE").setTitle("📶 `" + client.ws.ping + "ms`"));
   }
 }
