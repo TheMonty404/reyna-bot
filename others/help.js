1d4abf5b97d05fc4060b4a87d2e4316f43e108d4
AQUAMAN <78766821+shahrozxdr@users.noreply.github.com>
2021-03-19 20:08:10 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -42,6 +42,8 @@ module.exports = {
 >>>>>>>>>
 [{Support}](https://discord.gg/mwJmT7wkwn)
 [{Invite}](https://discord.com/api/oauth2/authorize?client_id=811298712348262470&permissions=8&scope=bot)
+    let helpEmbed = new MessageEmbed()
+    .setThumbnail(`https://cdn.discordapp.com/emojis/781510696265449493.gif `)
 `)
 
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
