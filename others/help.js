e45c5f45fcaffaed46faeb6affeffa5770903c23
AQUAMAN <78766821+shahrozxdr@users.noreply.github.com>
2021-03-19 20:13:48 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -42,9 +42,11 @@ module.exports = {
 >>>>>>>>>
 [{Support}](https://discord.gg/mwJmT7wkwn)
 [{Invite}](https://discord.com/api/oauth2/authorize?client_id=811298712348262470&permissions=8&scope=bot)
-    let helpEmbed = new MessageEmbed()
-    .setThumbnail("https://cdn.discordapp.com/emojis/781510696265449493.gif")
 `)
+    let helpEmbed = new MessageEmbed()
+    .setThumbnail(`https://cdn.discordapp.com/emojis/781510696265449493.gif `)
+    .setTitle(`**Rayal Help**`)
+    .setDescription(`
 
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
    .setColor("PURPLE");
