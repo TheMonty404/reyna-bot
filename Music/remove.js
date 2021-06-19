81c9ddc7640d4fabb182841c41cc06ab472fa26e
VISTx69 <78313191+VISTx69@users.noreply.github.com>
2021-06-19 23:45:49 +0300

    Update remove.js

--- Music/remove.js
+++ Music/remove.js
@@ -35,7 +35,7 @@ execute(message, args) {
     //send approve
     queue.textChannel.send(new MessageEmbed()
     .setDescription(`${message.author} removed **${song[0].title}** from the Queue`)
-    .setColor("PURPLE")
+    .setColor("RANDOM")
     );
   }
 };
