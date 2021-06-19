ad35d858ac43d24675a9731f9c2d5c0629717889
VISTx69 <78313191+VISTx69@users.noreply.github.com>
2021-06-19 23:22:49 +0300

    Update pause.js

--- Music/pause.js
+++ Music/pause.js
@@ -25,7 +25,7 @@ module.exports = {
       //pause the music
       queue.connection.dispatcher.pause(true);
       //define the pause embed
-      const pausemebed = new MessageEmbed().setColor("PURPLE")
+      const pausemebed = new MessageEmbed().setColor("RANDOM")
       .setAuthor(`${message.author.username} paused the music.`, "")
       //react with approve emoji
       message.react("<:emoji_4:822203026776391711>")
