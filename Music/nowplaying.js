b58803edbbb0b035d33e4bfc8df9a99143f293eb
VISTx69 <78313191+VISTx69@users.noreply.github.com>
2021-06-19 23:22:16 +0300

    Update nowplaying.js

--- Music/nowplaying.js
+++ Music/nowplaying.js
@@ -41,9 +41,9 @@ execute(message) {
     //define embed
     let nowPlaying = new MessageEmbed()
           .setTitle("**Now playing**")
-          .addField("<:emoji_4:822203026776391711> Requested by:", `\`${message.author.username}#${message.author.discriminator}\``, true)
-          .addField("<:emoji_3:822203003859107890> Length:", `\`${song.duration} Minutes\``, true)
-          .setColor("PURPLE")
+          .addField("👤 Requested by:", `\`${message.author.username}#${message.author.discriminator}\``, true)
+          .addField("⏱ Length:", `\`${song.duration} Minutes\``, true)
+          .setColor("RANDOM")
           
       //if its a stream
       if(ms >= 10000) {
