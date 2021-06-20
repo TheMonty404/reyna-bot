9177f977af690f4d7211ff28a92aca4cd7f2bb7e
VISTx69 <78313191+VISTx69@users.noreply.github.com>
2021-06-20 14:31:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -224,6 +224,35 @@ client.on("message", message => {
   }
 });
 
+client.on("guildCreate", guild => {
+  let channel = client.channels.cache.get("850779924352466984");
+  let embed = new MessageEmbed().setColor("#146DF6")
+  .setAuthor(client.user.username, client.user.avatarURL())
+  .setTitle( `✅ Join Server`)
+  .addField("🔠 **Server Name**", `${guild.name}`)
+  .addField("👑 **Server Owner**", `${guild.owner}`)
+  .addField("🆔 **Server Id**", `${guild.id}`)
+  .addField("👥 **Member Count**", `${guild.memberCount}`)
+  .setFooter(`${client.user.tag}`);
+  channel.send(embed);
+});
+
+
+client.on("guildDelete", guild => {
+  let channel = client.channels.cache.get("850779924352466984");
+  let embed = new MessageEmbed()
+  .setColor("#146DF6")
+  .setAuthor(client.user.username, client.user.avatarURL())
+  .setTitle( `❌ Left Server`)
+  .addField("🔠 **Server Name**", `${guild.name}`)
+  .addField("👑 **Server Owner**", `${guild.owner}`)
+  .addField("🆔 **Server Id**", `${guild.id}`)
+  .addField("👥 **Member Count**", `${guild.memberCount}`)
+  .setFooter(`${client.user.tag}`);
+  channel.send(embed);
+});
+
+
 client.on(`ready`, () => {	
 //////////////
 
