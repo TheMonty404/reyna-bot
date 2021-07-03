c10fae42c384b9c8e4e9d95e11a7712f043f19b0
Mooonty <82681184+Mooonty@users.noreply.github.com>
2021-07-03 19:10:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login("");
+client.login("ODMxMjEyMDg0MjcxNjQ0NzMz.YHR8lA.iM9lyzCwpGROt5Ya1WgMJezujVg");
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
