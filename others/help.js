31d051aaf72f5f966f961a7d85bc9d410446571e
Mooonty <82681184+Mooonty@users.noreply.github.com>
2021-07-24 20:49:54 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -26,28 +26,28 @@ module.exports = {
 \`tinvites\` - \`sug\` - \`roleuser\`
 \`report\`
 
-<a:music:852559405718175744> **Music Commands**
+<a:emoji_181:860644268900220938> **Music Commands**
 \`play\` - \`skip\` - \`skipto\`
 \`stop\` - \`volume\` - \`nowplaying\`
 \`shuffle\` - \`search\` - \`resume\`
 \`remove\` - \`queue\` - \`loop\`
 \`lyrics\` - \`radio\`
 
-<a:Work:852558706708054056> **Moderation Commands**
+<a:9FAF7C0CF0104A2F979F1263DF55D91A:855230044258828328> **Moderation Commands**
 \`ban\` - \`unban\` - \`setnick\`
 \`roleadd\` - \`roleremove\` - \`vkick\`
 \`setprefix\` - \`clear\` - \`close\`
 \`open\` - \`senddm\`
 
-<a:emoji_22:856127603345195019> **Gif Commands**
+<a:2716AA835D3841B89F82E038EEB22E82:832234494849187850> **Gif Commands**
 \`boy\` - \`girl\` - \`couple\`
 \`sad\` - \`cartoon\` - \`anime\`
 \`neon\`
 
-<a:Twitt3:852558825395453952> **Text Commands**
+<a:emoji_181:856878209554317332> **Text Commands**
 \`Comming Soon\`
 
-<a:emoji_20:856127714804105216> **Game Commands**
+<a:806D997882224ABDBC3552C77CF6EE3C:853287594003923014> **Game Commands**
 \`kiss\` - \`slap\` - \`hug\` - \`slots\`- \`weather\`
 
 <a:SIA_money:859103987889864734> **Economy Commands**
@@ -59,7 +59,7 @@ module.exports = {
 `)
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
    .setColor("RANDOM");
-   message.react("<a:53EC4B02339C41BF9A83F5597463DBEB:853287599720235029>")
+   message.react("<a:emoji_182:860935836240314378>")
     return message.channel.send(helpEmbed).catch(console.error);
 
   }
