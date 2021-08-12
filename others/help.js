afcf94ffcf7d3c127de6d277ead61f353213282b
Mooonty <82681184+Mooonty@users.noreply.github.com>
2021-08-12 15:37:27 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -18,21 +18,26 @@ module.exports = {
     .setThumbnail(`https://media.discordapp.net/attachments/861399487233654834/872561176456884224/image0.png `)
     .setTitle(`**Random Help**`)
     .setDescription(`
-> <a:emoji_180:856878164592427028>  **User Commands**
-> **__invite - support- about - ping - prefix - uptime - tinvites - sug - roleuser - report__**
+ <a:emoji_180:856878164592427028>  **User Commands**
+ **__invite - support- about - ping - prefix - uptime - tinvites - sug - roleuser - report__**
 
-> <a:emoji_102:871813008471507004> **Music Commands**
-> **__play - skip - skipto - stop - volume - nowplaying  shuffle - search  - loop - lyrics -radio__**
+ <a:emoji_102:871813008471507004> **Music Commands**
+ **__play - skip - skipto - stop - volume - nowplaying  shuffle - search  - loop - lyrics -radio__**
 
-> <a:FBBE600B13DB4F5986B4D0AF89247091:871811080148639794>  **Moderation Commands**
-> **__ban - unban - setnick - roleadd - roleremove - vkick - setprefix - clear - close - open - senddm__**
+ <a:emoji_11:845102475740577792> **Gif Commands**
+ **__boy - girl - couple - sad - cartoon - anime - neon__**
 
-> <a:kass:872897079305986108> **Game Commands**
-> **__kiss - slap - hug - slots - weather__**
+ <a:FBBE600B13DB4F5986B4D0AF89247091:871811080148639794>  **Moderation Commands**
+ **__ban - unban - setnick - roleadd - roleremove - vkick - setprefix - clear - close - open - senddm__**
 
-> <a:emoji_103:871813076964507710> **Links**
-> [Support](https://discord.gg/AkYA68Z2gG)
-> [Invite](https://discord.com/api/oauth2/authorize?client_id=831212084271644733&permissions=8&scope=bot)
+ <a:kass:872897079305986108> **Game Commands**
+ **__kiss - slap - hug - slots__**
+
+ <a:emoji_103:871813076964507710> **Links**
+ [Support](https://discord.gg/AkYA68Z2gG)
+ [Invite](https://discord.com/api/oauth2/authorize?client_id=831212084271644733&permissions=8&scope=bot)
+ [Website](https://randombotm.code.blog/)
+ [Sponsored](https://discord.gg/K8SSqweuSH)
 `)
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
    .setColor("RANDOM");
